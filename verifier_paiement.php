cc76acbc8c80a2a0bd39956c1c0d5f7b5413c9c1
pytharchimede <amani_ulrich@outlook.fr>
2025-02-23 19:29:17 +0000

    Add fontawesome

--- verifier_paiement.php
+++ verifier_paiement.php
@@ -7,6 +7,7 @@
     <title>Vérification de Transaction BNB</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 
 <body class="bg-dark text-light">
